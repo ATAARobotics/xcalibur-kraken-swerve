3eae6dfc36479c22643a1645d46866d618f6ff5a
Piaoyi Wu <piaoyiwu1@gmail.com>
2024-12-07 14:39:01 -0700

    potato networking web finally sustained

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -58,7 +58,7 @@ public class TunerConstants {
 
     // Theoretical free speed (m/s) at 12v applied output;
     // This needs to be tuned to your individual robot
-    public static final double kSpeedAt12VoltsMps = 4.73;
+    public static final double kSpeedAt12VoltsMps = 5.25;
 
     // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
@@ -72,7 +72,7 @@ public class TunerConstants {
     private static final boolean kInvertLeftSide = false;
     private static final boolean kInvertRightSide = true;
 
-    private static final String kCANbusName = "canivore";
+    private static final String kCANbusName = "rio";
     private static final int kPigeonId = 20;
 
 
